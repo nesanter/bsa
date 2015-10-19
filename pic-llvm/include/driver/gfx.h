42bcb26ae9107e7067e45a6e200c8fb3f5699110
Noah Santer <santerkrupp@gmail.com>
2015-10-19 19:58:00 -0400

    Forgot this too

---
+++ pic-llvm/include/driver/gfx.h
@@ -0,0 +1,10 @@
+#ifndef DRV_GFX_H
+#define DRV_GFX_H
+
+int drv_gfx_enable_write(int val, char * str);
+int drv_gfx_bb_write(int val, char * str);
+int drv_gfx_enqueue_write(int val, char * str);
+int drv_gfx_flush_write(int val, char * str);
+int drv_gfx_enqueue_read();
+
+#endif /* DRV_GFX_H */
